ff130e86d8a1eacdfa24a91d2d86b7a5013aa341
graefft <676@holbertonschool.com>
2019-09-11 20:49:08 +0000

    Fix: comment line

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Imports app_views and creates /status route on app_views
+''' Imports app_views and creates /status route on app_views'''
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
@@ -14,17 +14,17 @@ from models.user import User
 @app_views.route('/stats', strict_slashes=False)
 def stats():
     '''creates endpoint that retrieves number of each object by type'''
-    classes = {'amenities': storage.count(Amenity),
-               'cities': storage.count(City),
-               'places': storage.count(Place),
-               'reviews': storage.count(Review),
-               'states': storage.count(State),
-               'users': storage.count(User)}
+    classes = {'amenities':  storage.count(Amenity),
+               'cities':  storage.count(City),
+               'places':  storage.count(Place),
+               'reviews':  storage.count(Review),
+               'states':  storage.count(State),
+               'users':  storage.count(User)}
     return jsonify(classes)
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     '''returns JSON: "status": OK'''
-    result = {'status': 'OK'}
+    result = {'status':  'OK'}
     return jsonify(result), 200
