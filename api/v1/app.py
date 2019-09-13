c3c12000ffe228aa7a6e189643ef80eb9f231bad
komerela <kmwongera@icloud.com>
2019-09-13 07:00:28 +0000

    Added CORS security feature

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 '''Flask module to return status of API'''
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, session
 from models import storage
 from api.v1.views import app_views
 from os import getenv, environ
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
@@ -21,6 +23,7 @@ def clean_up(self):
 def not_found(e):
     '''returns not found on 404'''
     return jsonify(error="Not found"), 404
+
 
 
 if __name__ == '__main__':
